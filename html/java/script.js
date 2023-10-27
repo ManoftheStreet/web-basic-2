b1f72b100f0bf40198b10cd3c551a91742e42d1c
ManoftheStreet <zhijian6527@naver.com>
2023-10-27 12:07:34 +0900

    이름 넣기

--- html/java/script.js
+++ html/java/script.js
@@ -1,3 +1,11 @@
-function myExternalAlert(){
-      alert('자바스크립트 월드');
-    }
+function myExternalAlert() {
+  alert('자바스크립트 월드');
+}
+
+function submitName() {
+  var name = document.getElementById("nameInput").value;
+  if(name ==="")
+    alert(`이름을 입력해주세요`);
+  else 
+    alert(`안녕하세요! ${name} 님`);
+}
