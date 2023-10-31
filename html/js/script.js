a1a672ab732c6ea4f0bcafe3b137ec7e976fbd96
ManoftheStreet <zhijian6527@naver.com>
2023-10-31 11:32:13 +0900

    버튼색

--- html/js/script.js
+++ html/js/script.js
@@ -27,3 +27,31 @@ function submitNumber() {
     }
   }
 }
+function changeColor(color) {
+  var outputDiv = document.getElementById("outputName");
+  switch (color) {
+    case "빨강":
+      outputDiv.style.color = "red";
+      break;
+    case "파랑":
+      outputDiv.style.color = "blue";
+      break;
+    case "노랑":
+      outputDiv.style.color = "yellow";
+      break;
+    default:
+      break;
+  }
+}
+
+function submitColor() {
+  var name = document.getElementById("nameInput").value;
+  if (name === "") {
+    alert(`텍스트 값을 입력해 주세요`);
+  } else {
+    document.getElementById("outputName").innerHTML = `<p>${name}</p>
+            <button onclick="changeColor('빨강')">빨강</button>
+            <button onclick="changeColor('파랑')">파랑</button>
+            <button onclick="changeColor('노랑')">노랑</button>`;
+  }
+}
